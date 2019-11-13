d5c42b3881bed717c17c110afc7e8ff41228d858
Evgeniy Malyshev <sogol@bk.ru>
2019-11-13 12:38:42 +0300

    WebWaagh

---
+++ hw12.web/src/main/java/otus/hw_12/dbService/DBService.java
@@ -0,0 +1,22 @@
+package otus.hw_12.dbService;
+
+import otus.hw_12.entities.dataset.UserDataSet;
+
+import java.util.List;
+
+public interface DBService
+{
+    String getLocalStatus();
+
+    void save(UserDataSet dataSet);
+
+    UserDataSet read(long id);
+
+    UserDataSet readByName(String name);
+
+    List<UserDataSet> readAll();
+
+    List<Long> readAllIds();
+
+    void shutdown();
+}
